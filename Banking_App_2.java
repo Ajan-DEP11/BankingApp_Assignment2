9073ec8e48226baf818d7257b74a95fca1ba5148
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-17 09:31:09 +0530

    second commit

--- Banking_App_2.java
+++ Banking_App_2.java
@@ -5,6 +5,33 @@ public class Banking_App_2 {
     private static final Scanner scanner = new Scanner(System.in);
     
     public static void main(String[] args) {
+
+        final String CLEAR = "\033[H\033[2J";
+        final String COLOR_BLUE_BOLD = "\033[34;1m";
+        final String COLOR_RED_BOLD = "\033[31;1m";
+        final String COLOR_GREEN_BOLD = "\033[33;1m";
+        final String RESET = "\033[0m";
+
+        final String DASHBOARD = "Welcome to Smart Banking App";
+        final String NEW_ACCOUNT = "➕ Open New Account";
+        
+
+        final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
+        final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
+
+        String[] customerIds = new String[0];
+        String[] customerNames = new String[0];
+
+        String screen = DASHBOARD;
+
+
+        final String APP_TITLE = String.format("%s%s%s",
+        COLOR_BLUE_BOLD, screen, RESET);
+
+        System.out.println(CLEAR);
+        System.out.println("-".repeat(45));
+        System.out.println("\t" + APP_TITLE );
+        System.out.println("-".repeat(45));
         
     }
 }
