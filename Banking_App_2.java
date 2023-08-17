8c3f8267dedc4160e14a3e731a32dd25bc5e9caf
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-17 09:28:45 +0530

    First commit

---
+++ Banking_App_2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Banking_App_2 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+    }
+}
